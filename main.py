391b3d996ae8c5c611d138f1f6229dd9d770e478
Booorek <bartosz.sedzikowski31@gmail.com>
2025-02-15 12:50:53 +0100

    player with grouped actions

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
-        player.draw(screen)
-        player.update(dt)
+
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(165) / 1000
 
